f9ebfbcdcf6c9022d9b9745dfee0b69313f635e3
Larry Green <lgreen3@trinity.edu>
2022-10-02 07:57:37 -0500

    Login Page - Larry

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import logo from "./Images/cyberTexasLogo.png";
+import "./stylesLogin.css";
+
+
+
+// App() is a function to store and handle the user's data
+function App() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form"> 
+    
+      <form onSubmit={handleSubmit}>
+       
+        <div className="input-container">
+          
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  // This Is the Webpage Layout from the Title to the log in.
+  return (
+    <div className="app">
+      <div className="login-form">
+      <img src={logo} className="photo" centerImage="center" alt="This the main logo"/>
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/*Below is previous text from loading page*/
+
+/*import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
@@ -20,5 +119,5 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
