ed5788116edf7b80ca16831c6f1c86cab8681ae6
TheNameIsGav <spqr.dominus@gmail.com>
2022-10-11 14:16:31 -0500

    Test rework and documentation

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('true is truthy and false is falsy', () => {
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+
+  test('false is falsy', () => {
+    expect(false).toBe(false);
+  });
 });
+
+describe('Testing App.js', () => {
+  test('renders App component', () => {
+    render(<App />);
+  });
+});
+
+//Test documentation gained from https://www.robinwieruch.de/react-testing-library/
