6489120f5061b852d51c7a5316c8eadaa9e7f8db
TheNameIsGav <spqr.dominus@gmail.com>
2022-10-18 09:48:41 -0500

    Minor change to test framework

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,5 +1,5 @@
 const supertest = require("supertest");
-const app = require("../server/server");
+const app = require("../server/server.js");
 const chai = require('chai')
 const expect = chai.expect;
 
