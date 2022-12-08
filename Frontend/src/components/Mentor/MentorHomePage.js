60dd7971010e08296dd36a3c07813006ff190447
DESKTOP-D4A1412\larry <lgreen3@trinity.edu>
2022-12-08 15:26:05 -0600

    Latest Mentor Commit

--- Frontend/src/components/Mentor/MentorHomePage.js
+++ Frontend/src/components/Mentor/MentorHomePage.js
@@ -4,24 +4,6 @@ import "./stylesMentor.css"
 
 const MentorHome = () => {
 
-    const [query, setQuery] = useState("")
-
-    useEffect(() => {
-        var postData = ['national_id:', 'name', 'school', 'district', 'rotc' , 'active', 'coach']
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(postData)
-          };
-        fetch('/api/get-MentorData', requestOptions).then(res => res.json()).then(
-            data => {
-                setQuery(data.collection)
-                console.log("Values in data collection: " + data.collection)
-                console.log("Values in setElements: " + setQuery)
-                if (data.collection == null)
-                    console.log(Error)
-            })
-    },[]);
 
 return (
 
