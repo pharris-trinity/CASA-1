926c6f98169d474f27d09559aecfc2615515e0d8
DESKTOP-B01O21J\khali <khalinh101@gmail.com>
2022-12-08 16:11:40 -0600

    deleted some unnecessary variables & made comments

--- Frontend/src/components/App/App.js
+++ Frontend/src/components/App/App.js
@@ -9,7 +9,6 @@ import StudProfilePage from '../StudentPage/StudProfilePage';
 import StudentTakeAssessPage from '../StudentPage/StudTakeAssessPage';
 import StudentViewAssessPage from '../StudentPage/StudViewAssessPage';
 import Quiz1Test from '../StudentPage/Quiz1Test';
-import Quiz2Test from '../StudentPage/Quiz2Test';
 import QuizContent from '../StudentPage/QuizContent';
 
 import Mentorteamdisplay from "../TableMentor/MentorTable";
@@ -76,15 +75,7 @@ function App() {
         <Route path="stud/takeassess" element={<StudentTakeAssessPage/>} />
         <Route path="stud/viewassess" element={<StudentViewAssessPage/>} />
         <Route path="stud/takeassess/quiz1" element={<Quiz1Test/>} />
-        <Route path="stud/takeassess/quiz2" element={<Quiz2Test/>} />
-<<<<<<< ln3branch
         <Route path="stud/takeassess/quizcontent" element={<QuizContent/>} />
-        <Route path="mentor" element={<Mentor/>} />
-        <Route path="mentorteams" element={<MentorTeams/>} />
-        <Route path="mentorquiz" element={<MentorQuiz/>} />
-        <Route path="mentorassessment" element={<MentorAssessment/>} />
-=======
->>>>>>> develop
         <Route path="teacher" element={<Teacher/>} />
         <Route path="teacherteam" element={<TeacherTeams/>} />
         <Route path="teacherquiz" element={<TeacherQuiz/>} />
--- Frontend/src/components/StudentPage/StudMainPage.js
+++ Frontend/src/components/StudentPage/StudMainPage.js
@@ -3,7 +3,7 @@ import React from "react"
 import "./stylesStud.css"
 import StudNavbar from "./StudNavbar"
 
-
+/*the page for student main page; what shows up for on the main menu*/
 export default function StudentMainPage() {
     //local storage has current user information; parse it right by adding curly braces and get your json object
     const curruser = JSON.parse(localStorage.getItem("userID"));
