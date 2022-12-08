e3637d81854d343b208ede7f30cd0809757ac3e7
Amanda Choi <achoi@trinity.edu>
2022-12-08 13:13:25 -0600

    fixed bugs

--- Frontend/src/components/ViewTeams/ViewTeams.js
+++ Frontend/src/components/ViewTeams/ViewTeams.js
@@ -34,6 +34,33 @@ export default function ViewTeams2(){
         removesuccess: "Student Successfully Removed From Team"
     }
 
+
+    //getAllStudents gets all student accounts so addStudentAccount and removeStudentAccount can find a specific student 
+    const getAllStudents = (incText5) => {
+      //if (e && e.preventDefault) {e.preventDefault();}
+      //console.log(incText5);
+      const requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+      };
+      fetch ('/api/filter_students', requestOptions).then(res => res.text()).then(text => {
+        allStudents = JSON.parse(text);
+        //console.log(allStudents.length);
+        var tmp = 0;
+        while (tmp < allStudents.length){
+          //console.log(allStudents[tmp]._id);
+          //console.log(allStudents[tmp]);
+          if (allStudents[tmp]._id == incText5){
+            //console.log("match");
+            //console.log(allStudents[tmp].displayname);
+            addForDisplay(allStudents[tmp].displayname);
+            //addStudentAccount(0, allStudents[tmp]._id);
+          }
+          tmp++;
+        }
+      });
+    }
+
     //fetchUserAccount finds the team data to display
     //takes input of teamID and displays that team 
     const fetchUserAccount = (incText) => {
@@ -52,7 +79,23 @@ export default function ViewTeams2(){
                     setData1(userVal.school);
                     setData2(userVal.national_id);
                     setData3(userVal.members);
-                    
+                    //console.log(userVal.members[0]);
+                    var tmp = 0;
+                    console.log(userVal.members.length);
+                    while (tmp < userVal.members.length){
+                      console.log(userVal.members[tmp]);
+                      getAllStudents(userVal.members[tmp]);
+                      //console.log(allStudents[tmp]);
+                      //if (allStudents[tmp]._id == incText5){
+                        //console.log("match");
+                        //console.log(allStudents[tmp].displayname);
+                        //addForDisplay(allStudents[tmp].displayname);
+                        //addStudentAccount(0, allStudents[tmp]._id);
+                      //}
+                      tmp++;
+                    }
+            
+                    //fetchUserDisplayname(userVal.members[0]);
                 } catch (error) {
 
                 }
@@ -73,10 +116,16 @@ export default function ViewTeams2(){
       setJoinList(prev => prev.filter(joinList => joinList !== inputID));
     }
 
+    const removeProduct = () => {
+      setJoinList([
+        ...joinList.slice(0, 0),
+      ]);
+    }    
 
     //addStudentAccount takes in a team and a student username input and adds that student to that team or returns an error
     const addStudentAccount = (inputTeamID, inputStudentID) => {
           var tmpData = {team_id: inputTeamID, student_id: inputStudentID}
+          //console.log(inputTeamID);
           const requestOptions = {
               method: 'POST',
               headers: {'Content-Type': 'application/json'},
@@ -99,14 +148,17 @@ export default function ViewTeams2(){
                     setErrorMessages ({name: "user3", message:error.user3})
                   }
                   else{
-                    setJoinList(prev => [...prev, tmpData.student_id])
-                    console.log(setJoinList);
+                    setErrorMessages ({name: "addsuccess", message:error.addsuccess})
+                    addForDisplay(inputStudentID);
                   }
                 }
           );
     }; 
     //const[joinList, setJoinList] = useState([]);
 
+    const clearArray = (arr) => {
+        
+    }
 
     //removeStudentAccounts takes in a team and a student username inptut and removes that student from that team or returns an error
     const removeStudentAccount = (incText3, incText4) => {
@@ -133,10 +185,12 @@ export default function ViewTeams2(){
                       }
                       else{
                         setErrorMessages ({name: "removesuccess", message:error.removesuccess})
+                        removeForDisplay(incText4)
                       }
                 }
             );
     }; 
+
 
     const renderErrorMessage = (name) =>
         name === errorMessages.name && (
@@ -148,20 +202,35 @@ export default function ViewTeams2(){
     const [studInput, setstudInput] = useState('');
     const [studInput1, setstudInput1] = useState('');
 
-    
+    //546573742053747564656e74
+    //     
+    /*
+
+const names = ["whale", "squid", "turtle", "coral", "starfish"];
+
+const NamesList = () => (
+  <div>
+    <ul>{names.map(name => <li key={name}> {name} </li>)}</ul>
+  </div>
+);
+    */       
+
     return(
         <div className="App">
             <h1>Teams</h1>
             <input value={input} placeholder="enter team id" onChange={ev => setInput(ev.target.value)}/> 
-            <button onClick={()=>fetchUserAccount(input)}>Get Team</button>
+            <button onClick={()=>{removeProduct(); fetchUserAccount(input);}}>Get Team</button>
             <p>Team Name: {data}</p>
             <p>School: {data1}</p>
             <p>National Id: {data2}</p>
-            <p>Members: {joinList}</p>
-            
+            <p>Members: <ul>{joinList.map(name => <li key={name}>{name + "||"}</li>)}</ul></p>
+            <div>
+
+            </div>
+            <button onClick={()=>getAllStudents('6386376fd6b139afca5d08bf')}>Get Team</button>
             <div className="form-group">
               <input value={studInput} placeholder="enter student display name" onChange={ev1 => setstudInput(ev1.target.value)}/>
-              <button onClick={()=>{addStudentAccount(input, studInput); addForDisplay(studInput);}}>Add Student to Team</button> 
+              <button onClick={()=>{addStudentAccount(input, studInput)}}>Add Student to Team</button> 
               {renderErrorMessage("team")}
               {renderErrorMessage("user")}
               {renderErrorMessage("user2")}
@@ -171,7 +240,7 @@ export default function ViewTeams2(){
             </div>
             <div className="form-group">
               <input value={studInput1} placeholder="enter student display name" onChange={ev1 => setstudInput1(ev1.target.value)}/> 
-              <button onClick={()=>{removeStudentAccount(input, studInput1); removeForDisplay(studInput1);}}>Remove Student to Team</button>
+              <button onClick={()=>{removeStudentAccount(input, studInput1)}}>Remove Student to Team</button>
               {renderErrorMessage("removeteam")}
               {renderErrorMessage("removeuser")}
               {renderErrorMessage("removeuser2")}
