021678adb8cd81e47573e71f78f84d52a507eda3
ymatsumo23 <ymatsumo@trinity.edu>
2023-02-27 15:42:59 -0600

    cleaned up code, no functionality update

--- Frontend/src/components/Quiz/quiz.js
+++ Frontend/src/components/Quiz/quiz.js
@@ -5,50 +5,29 @@ import "./quiz.css"
 
 function Quiz(props) {
     const[questionArray, setQuestionArray] = useState([]);
-    const[questions,setQuestions] = useState([]);
     const[i, setI] = useState(0);
 
 
-    const pullOutQuestions = (dataOfQuiz) => {
-        console.log("quiz data access check: ", dataOfQuiz)
-        const tempArray = (dataOfQuiz.map(item => 
+    const pullOutQuestions = () => {
+        const tempArray = (props.quizData.map(item => 
             item.questions.map(secondItem => <Question questionData = {secondItem}/>)
-        )[0]);
-
-        const tempArray2 = dataOfQuiz.map(item => item.questions);
-        const tempArray3 = tempArray2.map(item => item);
-        console.log('Temp array 3 print', tempArray3);
-        console.log("Temp Array is printing!", tempArray);
-        setQuestionArray(tempArray);
+        ));
+        
+        setQuestionArray(...tempArray);
     }
 
     useEffect(() => {
-        pullOutQuestions(props.quizData);
+        pullOutQuestions();
         setI(0);
     }, [props.quizData]);
 
     console.log("questionArray", questionArray);
 
-    const tempArray = [];
-
-    useEffect(() => {
-        tempArray = (props.quizData.map(item => 
-            item.questions.map(secondItem => <Question questionData = {secondItem}/>)
-        )[i]);
-    }, [i]);
-
-    console.log("Questions after use effect", questions);
+    //console.log("Questions after use effect", questions);
 
     return (
         <div>
-            {/*props.quizData.map(item => 
-                <div>
-                    {item.questions.map(item2 => 
-                        <Question questionData = {item2}/>)[i]}
-                    </div>)[0]*/}
-            {tempArray}
-            
-
+            {questionArray && questionArray[i]}
             <Navigation/>
         </div>
     );
