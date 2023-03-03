293c588d71fc979ca0f339e1935833add6f94f33
ymatsumo23 <ymatsumo@trinity.edu>
2023-03-03 15:12:58 -0600

    moved creation of Answer of Component

--- Frontend/src/components/Quiz/question.js
+++ Frontend/src/components/Quiz/question.js
@@ -20,13 +20,12 @@ function Question(props) {
         return selectedAnswer == option;
     }
 
-    const makeAnswerComponents = () => {
-        answerComponentRef;
-        const tempArray = (props.questionData.answers.map((item) => <Answer answerText = {item} selection = {(e) => chooseAnswer(e)} ref = {answerComponentRef}/>));
-        
-        setAnswers(tempArray);
-        //console.log("answers array type", typeof tempArray);
-    }
+    // const makeAnswerComponents = () => {
+    //     answerComponentRef;
+    //     const tempArray = (props.questionData.answers.map((item) => <Answer answerText = {item} selection = {(e) => chooseAnswer(e)} ref = {answerComponentRef}/>));
+    //     setAnswers(tempArray);
+    //     //console.log("answers array type", typeof tempArray);
+    // }
     //console.log("Answers array:", answers);
     const checkAnswer = () => {
 
@@ -35,23 +34,23 @@ function Question(props) {
 
     //console.log("index: ", props.questionData.answers.indexOf("search"));
     useEffect(() => {
-
-        makeAnswerComponents();
+        //makeAnswerComponents();
     }, [props.questionData])
 
     useEffect(() => {
         console.log("you selected: ", selectedAnswer);
-        console.log("selected answer: ", props.questionData.answers.indexOf(selectedAnswer));
-        props.questionData.answers.map((item) => {
-            //console.log(answerComponentRef.current);
-            answerComponentRef.current.toggleSelection();
-        })
+        // console.log("selected answer: ", props.questionData.answers.indexOf(selectedAnswer));
+        // props.questionData.answers.map((item) => {
+        //     //console.log(answerComponentRef.current);
+        //     answerComponentRef.current.toggleSelection();
+        // })
     }, [selectedAnswer])
 
     return (
         <div>
             <p>{props.questionData.description}</p>
-            {answers}
+            {/*answers*/}
+            {props.questionData.answers.map((item) => <Answer onClick = {console.log(props.questionData.answers.indexOf(selectedAnswer))} answerText = {item} selection = {(e) => chooseAnswer(e)} ref = {answerComponentRef}/>)}
             {/*answers && console.log("index : ", answers)*/}
 
         </div>
