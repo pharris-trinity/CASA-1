da09c52b2f40b3ede21d27a507065a32114a3fc9
Josh <jrea1@trinity.edu>
2023-04-05 18:42:37 -0500

    Fixed the one issue with the CSS import

--- Frontend/src/components/General/QuizButtons.js
+++ Frontend/src/components/General/QuizButtons.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import pullOneQuiz from '../Student/StudTakeAssessPage';
-import './quizButtons.css'
+import './quizButtons.css';
 
-function QuizButtons({props}) {
+function QuizButtons(props) {
   return (
-    <div className='button-list'>
+    <div className="button-list">
       {props.quizlistInput.map((quiz) => (
-        <button className='my-button' key={quiz.id} onClick={() => pullOneQuiz(quiz.id)}>
+        <button className="my-button" key={quiz.id} onClick={() => pullOneQuiz(quiz.id)}>
           {quiz.name}
         </button>
       ))}
