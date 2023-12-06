595d33bb7624970239a218f08c61dc1f629dcf15
dpark-trinity <78444824+dpark-trinity@users.noreply.github.com>
2023-12-06 11:57:46 -0600

    SideBar_

--- Frontend/src/Resources/SideBar2/SideBar_.js
+++ Frontend/src/Resources/SideBar2/SideBar_.js
@@ -1,11 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './SideBar_.css'; // Import the styles
+import logo from "./../../Resources/logo.png"
+import LogoutButton from "./LogoutButton";
 // import profileImg from "../../Resources/profileimg.png"
 import profile_img from '../pngs/download.jpeg'
 
-const Sidebar = () => {
+const Sidebar = (props) => {
     
+    const buttonSetData = {
+        landing: [
+          { name: "CYBERTEXAS", link: "https://cybertexas.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT", link: "https://www.uscyberpatriot.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT RULEBOOK", link: "https://www.uscyberpatriot.org/competition/rules-book", target: "_blank", rel: "noopener noreferrer"},
+          { name: "LOGIN", link: "/login" }
+        ],
+        login: [
+          { name: "LOGIN", link: "/login" }
+        ],
+        logout: [
+          { name: "LOG OUT"}
+        ],
+        student: [ 
+          { name: "LOG OUT" }
+        ],
+        coach: [ 
+          { name: "CYBERTEXAS", link: "https://cybertexas.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT", link: "https://www.uscyberpatriot.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT RULEBOOK", link: "https://www.uscyberpatriot.org/competition/rules-book", target: "_blank", rel: "noopener noreferrer"},
+          { name: "LOG OUT"}
+        ],
+        mentor: [ 
+          { name: "CYBERTEXAS", link: "https://cybertexas.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT", link: "https://www.uscyberpatriot.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT RULEBOOK", link: "https://www.uscyberpatriot.org/competition/rules-book", target: "_blank", rel: "noopener noreferrer"},
+          { name: "LOG OUT"}
+        ],
+        admin: [ 
+          { name: "CYBERTEXAS", link: "https://cybertexas.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT", link: "https://www.uscyberpatriot.org/", target: "_blank", rel: "noopener noreferrer"},
+          { name: "CYBERPATRIOT RULEBOOK", link: "https://www.uscyberpatriot.org/competition/rules-book", target: "_blank", rel: "noopener noreferrer"},
+          { name: "LOG OUT"}
+        ],
+        takeQuiz: [ 
+          { name: "STUDENT HOME PAGE", link: "./../stud/main"},
+          { name: "LOG OUT"}
+        ]
+      };
+
+      const buttons = buttonSetData[props.buttonSet];
+
     useEffect(() => {
         // Dynamically add the stylesheet link to the document head
         const link = document.createElement('link');
@@ -19,6 +63,16 @@ const Sidebar = () => {
         };
     }, []); // Empty dependency array ensures this runs once on component mount
 
+    document.addEventListener('DOMContentLoaded', function () {
+        let btn = document.querySelector('#btn');
+        let sidebar = document.querySelector('.sidebar');
+      
+        btn.onclick = function () {
+          sidebar.classList.toggle('active');
+        };
+      });
+
+
   return (
     <div className="app-container">
         <div class="sidebar">
@@ -31,12 +85,13 @@ const Sidebar = () => {
                 <i class="bx bx-menu" id="btn"></i>
 
             </div>
-        <div class="user">
-            <img src={profile_img} alt="me" class="user-img" />
-            <div>
-                <p class="bold"> Coach Wilson </p>
-                <p> Admin </p>
-            </div>
+            
+            <div class="user">
+                <img src={profile_img} alt="me" class="user-img" />
+                <div>
+                    <p class="bold"> Coach Wilson </p>
+                    <p> Admin </p>
+                </div>
         </div>
         <ul>
             <li>
@@ -57,7 +112,7 @@ const Sidebar = () => {
 
             <li>
                 <a href="#">
-                    <i class="bx bxs-grid-alt"></i>
+                    <i class="bx bx-cog"></i>
                     <span class="nav-item"> Categories </span>
                 </a>
                 <span class="tooltip"> Categories </span>
@@ -65,14 +120,22 @@ const Sidebar = () => {
 
             <li>
                 <a href="#">
-                    <i class="bx bxs-grid-alt"></i>
-                    <span class="nav-item"> Orders </span>
+                    <i class="bx bx-log-out"></i>
+                    <span class="nav-item"> Logout </span>
                 </a>
-                <span class="tooltip"> Orders </span>
+                <span class="tooltip"> Logout </span>
             </li>
 
         </ul>
         </div>
+        
+        <div class="main-content">
+            <div class="container">
+                <h1> Code Commerce </h1>
+                <h1> Right Side </h1>
+            </div>
+        </div>
+
     </div>
   );
 }
